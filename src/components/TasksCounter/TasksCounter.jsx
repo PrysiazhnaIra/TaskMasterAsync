595a545b16bb85f981355c15049b73759628db07
PrysiazhnaIra <ira.prisyazhna@gmail.com>
2024-08-09 17:34:51 +0300

    logic to selectors

--- src/components/TasksCounter/TasksCounter.jsx
+++ src/components/TasksCounter/TasksCounter.jsx
@@ -1,23 +1,10 @@
 import css from "./TasksCounter.module.css";
 import { useSelector } from "react-redux";
-import { selectTasks } from "../../redux/selectors";
+import { selectTaskCount } from "../../redux/selectors";
 
 export default function TaskCounter() {
   // Отримуємо масив завдань із стану Redux
-  const tasks = useSelector(selectTasks);
-
-  // На базі стану Redux отримуємо похідні дані
-  const count = tasks.reduce(
-    (acc, task) => {
-      if (task.completed) {
-        acc.completed += 1;
-      } else {
-        acc.active += 1;
-      }
-      return acc;
-    },
-    { active: 0, completed: 0 }
-  );
+  const count = useSelector(selectTaskCount);
 
   return (
     <div className={css.counterBlock}>
