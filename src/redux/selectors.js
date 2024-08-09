a0fbf3bd7f2d8a06511c26e19bd46583f0095399
PrysiazhnaIra <ira.prisyazhna@gmail.com>
2024-08-09 21:56:39 +0300

    optimization

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,4 +1,5 @@
 import { statusFilters } from "./constants";
+import { createSelector } from "@reduxjs/toolkit";
 
 export const selectTasks = (state) => state.tasks.items;
 export const selectIsLoading = (state) => state.tasks.isLoading;
@@ -6,31 +7,29 @@ export const selectError = (state) => state.tasks.error;
 export const selectStatusFilter = (state) => state.filters.status;
 export const selectSearchFilter = (state) => state.filters.search;
 
-export const selectVisibleTasks = (state) => {
-  const tasks = selectTasks(state);
-  const statusFilter = selectStatusFilter(state);
-  const searchFilter = selectSearchFilter(state);
+export const selectVisibleTasks = createSelector(
+  [selectTasks, selectStatusFilter, selectSearchFilter],
+  (tasks, statusFilter, searchFilter) => {
+    // Фільтруємо завдання за статусом
+    const filteredTasks = tasks.filter((task) => {
+      switch (statusFilter) {
+        case statusFilters.active:
+          return !task.completed;
+        case statusFilters.completed:
+          return task.completed;
+        default:
+          return true;
+      }
+    });
 
-  // Фільтруємо завдання за статусом
-  const filteredTasks = tasks.filter((task) => {
-    switch (statusFilter) {
-      case statusFilters.active:
-        return !task.completed;
-      case statusFilters.completed:
-        return task.completed;
-      default:
-        return true;
-    }
-  });
+    // Додаємо пошуковий фільтр
+    return filteredTasks.filter((task) =>
+      task.text.toLowerCase().includes(searchFilter.toLowerCase())
+    );
+  }
+);
 
-  // Додаємо пошуковий фільтр
-  return filteredTasks.filter((task) =>
-    task.text.toLowerCase().includes(searchFilter.toLowerCase())
-  );
-};
-
-export const selectTaskCount = (state) => {
-  const tasks = selectTasks(state);
+export const selectTaskCount = createSelector([selectTasks], (tasks) => {
   const count = tasks.reduce(
     (acc, task) => {
       if (task.completed) {
@@ -44,4 +43,4 @@ export const selectTaskCount = (state) => {
   );
 
   return count;
-};
+});
